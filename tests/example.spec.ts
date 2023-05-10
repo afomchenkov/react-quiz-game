0caf3e5bf3944e24430c00c8a0c13f2822aacd01
famchankou <famchankou@gmail.com>
2023-05-10 08:40:48 +0200

    feat: update tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,113 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+import { generateRandomQuestions, questions } from './mock-api-response';
+import { QUESTIONS_URL } from '../src/constants';
 
-test('should render main page with question card', async ({ page }) => {
+const beforeEach = async (page: Page): Promise<void> => {
+  await page.route(QUESTIONS_URL, async route => {
+    const response = await generateRandomQuestions();
+    await route.fulfill({ json: { ...response } });
+  });
+
   await page.goto('http://localhost:3000/');
+}
 
-  await expect(page).toHaveTitle(/Quiz Game/);
+test.describe('Should test the quiz app', () => {
+  let answeredQuestions: string[] = [];
 
-  await page.locator('data-test-id=qz-nav-bar').waitFor();
-  await page.locator('data-test-id=qz-theme-toggler').waitFor();
-  await page.locator('data-test-id=qz-quiz-card').waitFor();
-  await page.locator('data-test-id=qz-form').waitFor();
+  test.beforeEach(async ({ page }) => {
+    answeredQuestions = [];
+  })
 
-  await page.locator('data-test-id=qz-score-bar').waitFor();
+  test('should render main page with question card and default score/chances values', async ({ page }) => {
+    await beforeEach(page);
+
+    await expect(page).toHaveTitle(/Quiz Game/);
+
+    await page.locator('data-test-id=qz-nav-bar').waitFor();
+    await page.locator('data-test-id=qz-theme-toggler').waitFor();
+    await page.locator('data-test-id=qz-quiz-card').waitFor();
+    await page.locator('data-test-id=qz-form').waitFor();
+
+    await page.locator('data-test-id=qz-score-bar').waitFor();
+    await page.locator('text=Score: 0').waitFor();
+    await page.locator('text=Chances: 3').waitFor();
+  });
+
+  test('should toggle the theme from dark to light and back', async ({ page }) => {
+    await beforeEach(page);
+
+    await page.locator('data-test-id=qz-nav-bar').waitFor();
+    const themeToggler = page.locator('data-test-id=qz-theme-toggler');
+    await expect(themeToggler).toContainText('Dark');
+    await themeToggler.click();
+    await expect(themeToggler).toContainText('Light');
+    await themeToggler.click();
+    await expect(themeToggler).toContainText('Dark');
+  });
+
+  test('should navigate to result page if three bad answers were provided', async ({ page }) => {
+    await beforeEach(page);
+
+    await page.locator('data-test-id=qz-nav-bar').waitFor();
+
+    const submitButton = page.locator('text=Submit');
+    const formInput = page.locator('data-test-id=qz-form-input');
+
+    await page.locator('text=Score: 0').waitFor();
+    await page.locator('text=Chances: 3').waitFor();
+
+    await formInput.fill('test-answer');
+    await submitButton.click();
+    await page.locator('text=Score: 0').waitFor();
+    await page.locator('text=Chances: 2').waitFor();
+
+    await formInput.fill('test-answer');
+    await submitButton.click();
+    await page.locator('text=Score: 0').waitFor();
+    await page.locator('text=Chances: 1').waitFor();
+
+    await formInput.fill('test-answer');
+    await submitButton.click();
+    await page.locator('text=Score: 0').waitFor();
+    await page.locator('text=Chances: 0').waitFor();
+
+    await page.locator('text=Your final result:').waitFor();
+    await page.locator('text=Gathered points: 0').waitFor();
+  });
+
+
+  test('should submit valid answers and no duplicates should be encountered', async ({ page }) => {
+    await beforeEach(page);
+
+    await page.locator('data-test-id=qz-nav-bar').waitFor();
+    const submitButton = page.locator('text=Submit');
+    const formInput = page.locator('data-test-id=qz-form-input');
+
+    await page.locator('text=Score: 0').waitFor();
+    await page.locator('text=Chances: 3').waitFor();
+
+
+    const submitAnswer = async () => {
+      const questionElement = page.locator('data-test-id=qz-quiz-question');
+      const question = await questionElement.textContent();
+
+      const { answerPlainText, answerSha1 } = questions.find(q => q.question === question)!;
+
+      if (answeredQuestions.includes(answerSha1)) {
+        throw new Error('Duplicate question found');
+      }
+
+      answeredQuestions.push(answerSha1);
+      await formInput.fill(answerPlainText);
+      await submitButton.click();
+    }
+
+    for (let i = 0; i < 30; ++i) {
+      await submitAnswer();
+    }
+
+    await page.locator('text=Score: 30').waitFor();
+    await page.locator('text=Chances: 3').waitFor();
+  });
 });
+
